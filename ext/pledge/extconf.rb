f3b34863635a2785b1d277627bca40bb5fd650e1
Jeremy Evans <code@jeremyevans.net>
2019-05-30 20:59:11 -0700

    Fix extconf, allow building with Ruby 2.7
    Ruby 2.7 no longer tolerates the incorrect argument to have_header.
    Also, if the pledge(2) call is not available, raise error during
    extconf.

--- ext/pledge/extconf.rb
+++ ext/pledge/extconf.rb
@@ -1,5 +1,6 @@
 require 'mkmf'
-have_header 'pledge'
+have_header 'unistd.h'
+have_func('pledge') || raise("pledge(2) not present, cannot built extension")
 $CFLAGS << " -O0 -g -ggdb" if ENV['DEBUG']
 $CFLAGS << " -Wall"
 create_makefile("pledge")
